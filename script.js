d6cf87127593e959d0dfaf1b022e872f81269d89
IsabellyFila <160731854+IsabellyFila@users.noreply.github.com>
2024-02-23 08:58:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const turnOff = document.getElementById('');
-const turnOff = document.getElementById('');
-const lamp = document.getElementById('');
+const turnOff = document.getElementById('ligar');
+const turnOff = document.getElementById('desligar');
+const lamp = document.getElementById('lamp');
 
+//como criar função
+
+function ligar (){
+ lamp.src = 'ligada.jpg'
+}
+
+turn0n.addEventListiner('click', Ligar);
